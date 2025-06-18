5e293167e3e4f55efe52734b9663c308a7daa185
sairamarava <sairamrdya@gmail.com>
2025-06-18 19:37:49 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
 //immport express
 import express from "express";
 import cors from "cors"; // Import CORS middlewar
-import dotenv from "dotenv";  
+import dotenv from "dotenv"; 
+import path from "path"; // Import path module 
 
 import {connectDB} from "./config/db.js";
 import router from "./routes/notesRoutes.js";
@@ -10,13 +11,16 @@ import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config(); // Load environment variables from .env file
 const PORT=process.env.PORT || 50001; // Set the port to 50001 or the value from .env
 //create an express app and listen on any port
+
+const __dirname = path.resolve();
 const app = express();
 
 ///middleware
-
-app.use(cors({
-  origin: "http://localhost:5173",
-}))
+if(process.env.NODE_ENV!=='production'){
+  app.use(cors({
+    origin: "http://localhost:5173",
+  }))
+}
 app.use(express.json()); // Parse JSON request bodies
 // // simple custom middleware 
 // app.use((req,res,next)=>{
@@ -25,6 +29,15 @@ app.use(express.json()); // Parse JSON request bodies
 // })
 app.use(rateLimiter)
 app.use("/api/notes", router);
+
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+  app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+})
+}
+
 connectDB().then(()=>{
 app.listen(PORT, () => {
   console.log("Server started as port",PORT);
